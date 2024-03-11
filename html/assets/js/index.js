fee0fd2cabf1816346141a2bb603037cad14fc82
Louis Rodet <louis.rodet@epita.fr>
2024-03-11 22:21:41 +0530

    auto

--- html/assets/js/index.js
+++ html/assets/js/index.js
@@ -1,6 +1,14 @@
+function loadcolors(jsonData) {
+    document.documentElement.style.setProperty('--primary-color', jsonData.colors.primarycolor);
+    document.documentElement.style.setProperty('--secondary-color', jsonData.colors.secondarycolor);
+    document.documentElement.style.setProperty('--third-color', jsonData.colors.thirdcolor);
+}
+
 function aboutMeCard(jsonData) {
     aboutMeDiv = document.createElement('div');
     aboutMeDiv.classList.add('aboutme');
+    aboutMeDiv.classList.add('small-card');
+    aboutMeDiv.style.backgroundImage = `url('${jsonData.aboutme.banner}')`;    
 
     pp = document.createElement('div');
     pp.classList.add('profile-picture-div');
@@ -28,16 +36,15 @@ function aboutMeCard(jsonData) {
     contactList = document.createElement('ul');
     contactList.classList.add('contact-list');
 
-    for (var i = 0; i < jsonData.contact.length; i++) {
+    for (var i = 0; i < jsonData.aboutme.contact.length; i++) {
         li = document.createElement('li');
-        li.setAttribute('onclick', `window.open('${jsonData.contact[i].link}', '_blank')`);
-        li.setAttribute('description', jsonData.contact[i].description);
-        //li.title = jsonData.contact[i].description;
+        li.setAttribute('onclick', `window.open('${jsonData.aboutme.contact[i].link}', '_blank')`);
+        li.setAttribute('description', jsonData.aboutme.contact[i].description);
         link = document.createElement('div');
         link.classList.add('contact-element-logo');
         
         li.classList.add('contact-element');
-        link.style.backgroundImage = `url('${jsonData.contact[i].logo}')`;
+        link.style.backgroundImage = `url('${jsonData.aboutme.contact[i].logo}')`;
         link.style.backgroundSize = 'cover';
         li.appendChild(link);
         contactList.appendChild(li);
@@ -47,10 +54,40 @@ function aboutMeCard(jsonData) {
     return aboutMeDiv;
 }
 
-function loadcolors(jsonData) {
-    document.documentElement.style.setProperty('--primary-color', jsonData.colors.primarycolor);
-    document.documentElement.style.setProperty('--secondary-color', jsonData.colors.secondarycolor);
-    document.documentElement.style.setProperty('--third-color', jsonData.colors.thirdcolor);
+function loadformation(jsonData) {
+    formationDiv = document.createElement('div');
+    formationDiv.classList.add('small-card');
+    formationDiv.classList.add('formation');
+
+    titleformation = document.createElement('h2');
+    titleformation.classList.add('formation-title');
+    titleformation.textContent = jsonData.formation.name;
+    formationDiv.appendChild(titleformation);
+
+    for (var i = 0; i < jsonData.formation.formation.length; i++) {
+        let school = document.createElement('div');
+        school.classList.add('school');
+
+        let schoologo = document.createElement('div');
+        schoologo.classList.add('schoologo');
+        schoologo.style.backgroundImage = `url('${jsonData.formation.formation[i].logo}')`;
+        school.appendChild(schoologo);
+
+        let schoolblabla = document.createElement('div');
+        schoolblabla.style = 'display:inline-block; margin: 0 10px; vertical-align: middle;';
+        school.appendChild(schoolblabla);
+        
+        let schoolname = document.createElement('h4');
+        schoolname.style.margin = '0';
+        schoolname.textContent = jsonData.formation.formation[i].name;
+        schoolblabla.appendChild(schoolname);
+        let schooldate = document.createElement('p');
+        schooldate.style.margin = '0';
+        schooldate.textContent = `${jsonData.formation.formation[i].begin} - ${jsonData.formation.formation[i].end}`;
+        schoolblabla.appendChild(schooldate);
+        formationDiv.appendChild(school);
+    }
+    return formationDiv;
 }
 
 
@@ -71,6 +108,7 @@ function loader() {
             .then(jsonData => {
                 loadcolors(jsonData);
                 rootElement.appendChild(aboutMeCard(jsonData));
+                rootElement.appendChild(loadformation(jsonData));
             })
             .catch(error => console.error('Erreur lors du chargement du fichier JSON:', error));
     });
